21c2e10f6c49e9b0c4aa682596ca3cd9f7b310b8
jedachi <jesuschirre@gmail.com>
2025-01-21 11:34:32 -0500

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className='x2-flex'>
+      <div className='header'>
+          <nav> 
+            <a href="#" className='logo'>logo puta</a>
+            <a href="#">la mas puta</a>
+            <a href="#">putita</a> 
+            <a href="">putita</a><a href="">putita</a><a href="">putita</a><a href="">putita</a><a href="">putita</a>
+          </nav>
+      </div>
+      <main>
+        <section>
+          <p>Este es el contenido de ejemplo para mi proyecto.</p>
+        </section>
+      </main>
+    </div>
+
+        <footer>
+          <p>&copy; 2025 Mi Proyecto</p>
+        </footer>
+    </>
+  )
+}
+
+export default App
